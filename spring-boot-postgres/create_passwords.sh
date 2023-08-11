f4bc18b4caf3eca52256d2a9d557be18cad737e5
Evan Oslick <eoslick@users.noreply.github.com>
2023-08-11 13:55:02 -0400

    Update create_passwords.sh

--- spring-boot-postgres/create_passwords.sh
+++ spring-boot-postgres/create_passwords.sh
@@ -7,14 +7,12 @@ export POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD
 export POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD
 
 # Update 01.sql and 02_password_update.sql files with the passwords
-sed -i.bak "s|'VAADIN_PASSWORD'|'$POSTGRES_DB_PASSWORD'|g" ./sql/01.sql
-sed -i.bak "s|'POSTGRES_PASSWORD'|'$POSTGRES_ROOT_PASSWORD'|g" ./02_password_update.sql
-sed -i.bak "s|'VAADIN_PASSWORD'|'$POSTGRES_DB_PASSWORD'|g" ./02_password_update.sql
+sed -i.bak "s|'POSTGRES_DB_USER'|'$POSTGRES_DB_USER'|g" ./sql/01.sql
+sed -i.bak "s|'POSTGRES_DB_PASSWORD'|'$POSTGRES_DB_PASSWORD'|g" ./sql/01.sql
 
 # Remove backup files
 rm ./sql/01.sql.bak
-rm ./02_password_update.sql.bak
 
 # Store passwords into .env file
-echo "POSTGRES_PASSWORD=$POSTGRES_DB_PASSWORD" > .env
-echo "VAADIN_PASSWORD=$POSTGRES_DB_PASSWORD" >> .env
+echo "POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD" >> .env
+echo "POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD" >> .env
